e0d0105dbe92e46164498923a66ecc62ea22e945
Kranium Gikos Mendoza <kranium@gikos.net>
2016-01-21 23:49:23 +0800

    cleaned up script with some hints from shellcheck

--- default.nix
+++ default.nix
@@ -1,11 +1,10 @@
-# nix-build -E 'with import <nixpkgs> { }; callPackage ./default.nix { }'
-{ stdenv, pkgs, coreutils, curl, icedtea8_web, openjdk8, patchelf, xmlstarlet }:
+{ stdenv, pkgs, curl, icedtea8_web, openjdk8, patchelf, xmlstarlet }:
 
 assert stdenv.system == "x86_64-linux";
 
 let
 
-  run = pkgs.writeScript "laucnh-ikvm" ''
+  run = pkgs.writeScript "launch-ikvm" ''
     #!/usr/bin/env bash
 
     if [ -z "$1" ]; then
@@ -17,13 +16,12 @@ let
     fi
 
     XPATH_JAR_RESOURCES='/jnlp/resources[@os="Linux" and (@arch="amd64" or @arch="x86_64")]'
-    XPATH_JAR_RESOURCES_DELETE='/jnlp/resources[@os="Linux"]'
 
     JAR_FILE=$(${xmlstarlet}/bin/xml sel -t -v "$XPATH_JAR_RESOURCES[1]/nativelib/@href" "$1")
 
     CURL_BASE=$(${xmlstarlet}/bin/xml sel -t -v '/jnlp/@codebase' "$1")
 
-    TMPDIR=$(${coreutils}/bin/mktemp -d)
+    TMPDIR=$(mktemp -d)
 
     cd "$TMPDIR" || exit 1
 
@@ -31,14 +29,13 @@ let
     ${openjdk8}/bin/unpack200 "$JAR_FILE.pack.gz" "$JAR_FILE"
     ${openjdk8}/bin/jar xf "$JAR_FILE"
 
-
     for i in libiKVM64.so libSharedLibrary64.so
     do
       ${patchelf}/bin/patchelf --set-rpath ${stdenv.cc.cc}/lib $i
     done
 
-    LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`
-    ${xmlstarlet}/bin/xml ed -d "$XPATH_JAR_RESOURCES_DELETE" "$1" > new-launch.jnlp
+    LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)
+    ${xmlstarlet}/bin/xml ed -d "$XPATH_JAR_RESOURCES" "$1" > new-launch.jnlp
     ${icedtea8_web}/bin/javaws new-launch.jnlp
 
     rm -rf "$TMPDIR"
